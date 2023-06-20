6297372153aa83635d7e0ed9237907baab11edf2
unknown <ibrahimnaseer896@gmail.com>
2023-06-20 03:49:20 -0700

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Add the following JavaScript code to handle the hamburger menu functionality
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
-const burger = document.querySelector('.burger');
 
-burger.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
